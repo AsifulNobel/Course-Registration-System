e40da1f418eb206625a5173f4760dc1068fbe385
Shawon Ashraf <shawon13@live.com>
2017-03-03 23:09:16 +0600

    Implemented RegistrationControllerClass

--- src/assignment/controller/RegisterCourseController.java
+++ src/assignment/controller/RegisterCourseController.java
@@ -1,4 +1,28 @@
 package assignment.controller;
 
+import assignment.models.Course;
+import assignment.models.Registration;
+
 public class RegisterCourseController {
+    private Registration reg;
+
+    public void makeNewRegistration() {
+        reg = new Registration();
+    }
+
+    public void addCourse(Course course) {
+        reg.addCourse(course);
+    }
+
+    public Registration getReg() {
+        return reg;
+    }
+
+    public Course getCourse(String id) {
+        Course course;
+
+        course = reg.getCourseList().stream().filter(k -> k.getId().equals(id)).findFirst().get();
+
+        return course;
+    }
 }
