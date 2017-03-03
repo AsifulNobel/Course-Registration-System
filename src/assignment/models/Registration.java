10f68e5681c976a0c0a681ee5ab926c4ef978b3b
Shawon Ashraf <shawon13@live.com>
2017-03-03 22:36:00 +0600

    Implemented Registration class

--- src/assignment/models/Registration.java
+++ src/assignment/models/Registration.java
@@ -1,7 +1,36 @@
 package assignment.models;
 
+import java.util.LinkedList;
+
 /**
- * Created by nobel on 03/03/17.
+ * Created by nobel
+ * modified by shawon
+ * on 03/03/17.
  */
 public class Registration {
+    public LinkedList<Course> courseList;
+
+    public Registration() {
+         courseList = new LinkedList<>();
+    }
+
+    public LinkedList<Course> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(LinkedList<Course> courseList) {
+        this.courseList = courseList;
+    }
+
+    public void addCourse(Course newCourse) {
+
+    }
+
+    public int getTotal() {
+        int total = 0;
+
+        total = courseList.stream().mapToInt(Course::getSubTotal).sum();
+
+        return total;
+    }
 }
