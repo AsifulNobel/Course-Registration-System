41a53a7d1999044a65823c7939f416608147653a
Shawon Ashraf <shawon13@live.com>
2017-03-04 00:19:35 +0600

    Added Test for Course Class

---
+++ src/assignment/tests/CourseTest.java
@@ -0,0 +1,19 @@
+package assignment.tests;
+
+
+import assignment.models.Course;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by shawon on 3/4/17.
+ */
+public class CourseTest {
+
+    @Test
+    public void getSubTotalTest() {
+        Course course = new Course("CSE 327", "Soft.Eng.", 3, 5500);
+        int subTotal = course.getSubTotal();
+        Assert.assertEquals((3 * 5500), subTotal);
+    }
+}
--- src/assignment/tests/GetCourseTest.java
+++
@@ -1,9 +0,0 @@
-package assignment.tests;
-
-
-
-/**
- * Created by shawon on 3/4/17.
- */
-public class GetCourseTest {
-}
