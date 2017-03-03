d1b2329dd2a7df3699d43df53035feb5b857d85d
Shawon Ashraf <shawon13@live.com>
2017-03-04 00:50:45 +0600

    Modified CourseFactory class

--- src/assignment/models/CourseFactory.java
+++ src/assignment/models/CourseFactory.java
@@ -20,6 +20,8 @@ public class CourseFactory {
         cList.add(new Course("CSE 115", "Prog.Lang.1", 4, 5500));
         cList.add(new Course("CSE 173", "Disc.Math", 3, 5500));
         cList.add(new Course("CSE 215", "Prog.Lang.2", 4, 5500));
+        cList.add(new Course("CSE 311", "Database", 3, 5500));
+        cList.add(new Course("CSE 338", "Networking", 4, 5500));
 
     }
 
