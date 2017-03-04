22d6be0679f8fe8b24538d25c20f396a30ff931b
Shawon Ashraf <shawon13@live.com>
2017-03-04 18:07:15 +0600

    Added Button handlers and slNO generator

--- src/assignment/controller/RegistrationFxController.java
+++ src/assignment/controller/RegistrationFxController.java
@@ -2,13 +2,17 @@ package assignment.controller;
 
 import assignment.models.Course;
 import assignment.models.CourseFactory;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,13 +23,19 @@ import java.util.ResourceBundle;
 public class RegistrationFxController implements Initializable {
     @FXML private TableView<Course> table;
     @FXML private TableColumn<Course, String> title;
+    @FXML private TableColumn<Course, Integer> slNo;
     @FXML private TableColumn<Course, Integer> credit;
     @FXML private TableColumn<Course, Integer> tuitionPerCredit;
+    @FXML private TableColumn<Course, Integer> subTotal;
+
+    @FXML private Button addButton;
+    @FXML private Button newReg;
+
+    @FXML private TextField courseField;
+
+    private RegisterCourseController controller;
 
     public ObservableList<Course> data = FXCollections.observableArrayList(
-//            new Course("CSE 327", "Soft.Eng.", 3, 5500),
-//            new Course("CSE 440", "Artificial Intel.", 3, 5500)
-
             new CourseFactory().getcList()
     );
 
@@ -34,7 +44,35 @@ public class RegistrationFxController implements Initializable {
         title.setCellValueFactory(new PropertyValueFactory<Course, String>("title"));
         credit.setCellValueFactory(new PropertyValueFactory<Course, Integer>("credit"));
         tuitionPerCredit.setCellValueFactory(new PropertyValueFactory<Course, Integer>("tuitionPerCredit"));
+        subTotal.setCellValueFactory(new PropertyValueFactory<Course, Integer>("subTotal"));
 
         table.setItems(data);
+        slNo.setCellValueFactory(col -> new ReadOnlyObjectWrapper<Integer>(table.getItems().indexOf(col.getValue()) + 1));
+        slNo.setSortable(false);
+
+        controller = new RegisterCourseController();
+    }
+
+    @FXML
+    public void addReg() {
+        // set the table empty
+        // setting ObersevableList empty clears the table
+        data.clear();
+        controller.makeNewRegistration();
+    }
+
+    @FXML
+    public void addCourse() {
+        CourseFactory factory = new CourseFactory();
+        String courseID = courseField.getText();
+
+        controller.addCourse(factory.getCourse(courseID));
+        Course courseToAdd = controller.getCourse(courseID);
+
+        if(courseID != null) {
+            data.add(courseToAdd);
+        } else {
+            System.out.println("Course not found");
+        }
     }
 }
