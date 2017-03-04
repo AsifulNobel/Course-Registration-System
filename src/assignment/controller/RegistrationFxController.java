a7951b672ae7d57f1b68e40896d8a9a89975a271
AsifulNobel <asif.nobel@gmail.com>
2017-03-04 17:49:52 +0600

    TableView Fix

--- src/assignment/controller/RegistrationFxController.java
+++ src/assignment/controller/RegistrationFxController.java
@@ -2,6 +2,8 @@ package assignment.controller;
 
 import assignment.models.Course;
 import assignment.models.CourseFactory;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -9,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,22 +21,23 @@ import java.util.ResourceBundle;
  */
 public class RegistrationFxController implements Initializable {
     @FXML private TableView<Course> table;
+    @FXML private TableColumn<Course, Integer> serial_no;
     @FXML private TableColumn<Course, String> title;
     @FXML private TableColumn<Course, Integer> credit;
     @FXML private TableColumn<Course, Integer> tuitionPerCredit;
+    @FXML private TableColumn<Course, Integer> subTotal;
 
     public ObservableList<Course> data = FXCollections.observableArrayList(
-//            new Course("CSE 327", "Soft.Eng.", 3, 5500),
-//            new Course("CSE 440", "Artificial Intel.", 3, 5500)
-
             new CourseFactory().getcList()
     );
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        serial_no.setCellValueFactory(column -> new ReadOnlyObjectWrapper<Integer>(table.getItems().indexOf(column.getValue())+1));
         title.setCellValueFactory(new PropertyValueFactory<Course, String>("title"));
         credit.setCellValueFactory(new PropertyValueFactory<Course, Integer>("credit"));
         tuitionPerCredit.setCellValueFactory(new PropertyValueFactory<Course, Integer>("tuitionPerCredit"));
+        subTotal.setCellValueFactory(new PropertyValueFactory<Course, Integer>("subTotal"));
 
         table.setItems(data);
     }
