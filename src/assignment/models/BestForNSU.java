93488239628c5ec1de8754b13016651129c0752b
ShawonAshraf <shawon13@live.com>
2017-03-20 20:07:23 +0600

    Added BestForNSU Class

---
+++ src/assignment/models/BestForNSU.java
@@ -0,0 +1,11 @@
+package assignment.models;
+
+/**
+ * Created by shawonashraf on 3/20/17.
+ */
+public class BestForNSU extends CompositeDiscount {
+    @Override
+    public int getTotal(Registration reg) {
+        return super.getTotal(reg);
+    }
+}
