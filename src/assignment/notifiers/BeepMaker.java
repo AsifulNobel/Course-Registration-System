28decc5fb68ec605d86012749984cff15ea2b438
ShawonAshraf <shawon13@live.com>
2017-03-28 02:58:51 +0600

    copied beep.wav to notifiers package to be picked up by classLoader

--- src/assignment/notifiers/BeepMaker.java
+++ src/assignment/notifiers/BeepMaker.java
@@ -1,10 +1,13 @@
 package assignment.notifiers;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import sun.audio.AudioPlayer;
 import sun.audio.AudioStream;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.net.URI;
 
 /**
  * Created by nobel on 21/03/17.
@@ -17,7 +20,6 @@ public class BeepMaker implements Runnable {
     }
 
     private void playBeep() {
-        String filePath = "resources/beep.wav";
 
     }
 
